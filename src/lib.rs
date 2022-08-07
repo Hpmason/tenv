cf4542d231a973d49df1d7e76a7e865d4396ed44
Mason Ginter <mason@dagint.com>
2022-08-07 18:54:04 -0400

    PATH additions (#5)
    * Support path additions as CLI args
    
    * Remove unused Error type
    
    * Add doc comment
    
    * Capitalize arg descriptions
    
    * add path additions to beginning of PATH
    
    This way, path additions have precedence over original PATHs

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,30 @@
 use std::{
     collections::HashMap,
-    io,
+    env, fs, io,
     process::{Command, ExitStatus},
 };
 
 use clap::Parser;
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum TEnvError {
-    #[error("unable to parse environment variables")]
-    ParseEnvironmentVariables,
-    #[error("missing command")]
-    MissingCommand,
-    #[error("error running {0}")]
-    CommandRunError(#[from] io::Error),
-    #[error("{0}")]
-    Other(String),
-}
 
 #[derive(Debug, Clone, Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct CommandArgs {
-    /// list of env var assignments
+    /// List of env var assignments to be set when running program
     #[clap(short('e'), value_parser(parse_key_val::<String, String>))]
     env_vars: Vec<(String, String)>,
-    /// program being run
+    /// Entries to add to the path variable when running program
+    #[clap(short('p'))]
+    path_additions: Vec<String>,
+    /// Program being run
     #[clap(required(true))]
     program: String,
-    /// args for program to be run
+    /// Args for program to be run
     #[clap(last(true))]
     args: Vec<String>,
 }
 
 /// Parse a single key-value pair
-/// taken from https://docs.rs/clap/latest/clap/_derive/_cookbook/typed_derive/index.html
+/// taken from <https://docs.rs/clap/latest/clap/_derive/_cookbook/typed_derive/index.html>
 fn parse_key_val<T, U>(
     s: &str,
 ) -> Result<(T, U), Box<dyn std::error::Error + Send + Sync + 'static>>
@@ -51,38 +41,67 @@ where
 }
 
 /// Runs command, while setting environment variables before unsets them after command is completed
-pub fn run(command_args: CommandArgs) -> Result<ExitStatus, io::Error> {
-    let hash_map_args: HashMap<String, String> =
-        HashMap::from_iter(command_args.env_vars.clone().into_iter());
-    let status = Command::new(&command_args.program)
-        .args(&command_args.args)
-        .envs(hash_map_args)
-        .status();
+pub fn run(command_args: &CommandArgs) -> Result<ExitStatus, io::Error> {
+    // Convert Vec of env vars to HashMap
+    let hash_map_vars: HashMap<String, String> =
+        command_args.env_vars.clone().into_iter().collect();
+    
+    // Get shell and appropriate flag to run command through OS shell
+    let (shell, flag) = get_shell_and_flag();
+    // Combine flag with program and its args [flag, program name, rest of args]
+    let final_args = {
+        let mut args=  vec![flag.to_string(), command_args.program.clone()];
+        args.extend(command_args.args.clone());
+        args
+    };
 
-    // Check if error running command
-    if status.is_err() {
-        // If error, try running it as a system command
-        if matches!(status.as_ref().unwrap_err().kind(), io::ErrorKind::NotFound) {
-            return run_as_system_command(&command_args);
-        }
+    // Build command with shell
+    let mut command = Command::new(shell);
+    // Add args for shell to run command
+    command.args(&final_args)
+        // Set env variables
+        .envs(hash_map_vars);
+    // If path_additions passed to CLI, get and set to new path
+    if !command_args.path_additions.is_empty() {
+        // Generate new PATH
+        let new_path = get_appended_path(&command_args.path_additions);
+        // Set PATH env var
+        command.env("PATH", new_path);
     }
-    status
+    
+    // Run command and return status 
+    command.status()
 }
 
-/// If not found as a binary, try running it through cmd or bash directly
-fn run_as_system_command(command_args: &CommandArgs) -> Result<ExitStatus, io::Error> {
-    // Get shell and command line argument to run command through shell
-    let (shell, flag) = if cfg!(windows) {
+/// Generate new PATH from appending path additions to existing PATH 
+fn get_appended_path(path_additions: &[String]) -> String {
+    // Canonicalize paths so we can add to PATH
+    let mut path_additions: Vec<String> = path_additions
+        .iter()
+        .flat_map(|s| {
+            fs::canonicalize(s)
+                .map(|can_path| can_path.to_string_lossy().to_string())
+        })
+        .collect();
+    // get original path variable
+    let original_path: Vec<String> = env::split_paths(&env::var("PATH").unwrap_or_default())
+        // Convert paths to String
+        .map(|path| path.to_string_lossy().to_string())
+        .collect();
+
+    // Add out additions to the beginning of the PATH
+    path_additions.extend(original_path);
+    
+    // join paths to get our new PATH environment variable
+    let new_path = env::join_paths(path_additions).expect("could not join paths");
+    new_path.to_string_lossy().to_string()
+}
+
+/// Return ("powershell", "-Command") for windows or ("bash", "-c") for any other OS
+const fn get_shell_and_flag<'a>() -> (&'a str, &'a str) {
+    if cfg!(windows) {
         ("powershell", "-Command")
     } else {
         ("bash", "-c")
-    };
-
-    let mut args: Vec<String> = vec![flag.to_string(), command_args.program.clone()];
-    args.extend_from_slice(&command_args.args);
-    
-    let hash_map_env: HashMap<String, String> =
-        HashMap::from_iter(command_args.env_vars.clone().into_iter());
-    let status = Command::new(shell).args(args).envs(hash_map_env).status();
-    status
+    }
 }
