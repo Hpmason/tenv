ba731b115c38b6f77f00d4a38aa6593c1e794b33
Mason Ginter <mason@dagint.com>
2022-08-07 19:27:56 -0400

    Bug #6: Fix bug with starship in powershell (#7)
    * #6: fix ctrl+c with starship

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use clap::Parser;
-use tenv::{run, CommandArgs};
+use tenv::{init_ctrlc_handler, run, CommandArgs};
 
 fn main() {
     // Get CLI args
     let args: CommandArgs = CommandArgs::parse();
+    // Initialize control+C handler. If error, just let user know it failed to set handler
+    if init_ctrlc_handler().is_err() {
+        println!("tenv: could not set ctrl+c handler, so issues may arise if ctrl+c is entered");
+    }
     // Run program and print error if there is one
     if let Err(e) = run(&args) {
         println!("Error running command with TEnv: {e}");
